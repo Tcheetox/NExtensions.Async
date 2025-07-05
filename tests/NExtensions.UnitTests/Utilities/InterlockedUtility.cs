9b6fda7b919b949ef34332469ae87c3c6be20136
Tcheetox <tcheetox@gmail.com>
2025-07-05 09:54:09 +0200

    ut: one more unit test for asyncreaderwriterlock

---
+++ tests/NExtensions.UnitTests/Utilities/InterlockedUtility.cs
@@ -0,0 +1,16 @@
+﻿namespace NExtensions.UnitTests.Utilities;
+
+internal static class InterlockedUtility
+{
+	public static void Max(ref int target, int value)
+	{
+		int initialValue, computedValue;
+		do
+		{
+			initialValue = Volatile.Read(ref target);
+			computedValue = Math.Max(initialValue, value);
+			if (initialValue == computedValue)
+				return; // Already at or above value
+		} while (Interlocked.CompareExchange(ref target, computedValue, initialValue) != initialValue);
+	}
+}
